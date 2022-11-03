3c5a3531abade107c4928be0689ba8842ceec655
Fernando <fernando.gomez@netpay.com.mx>
2022-11-02 21:25:08 -0600

    Rating select component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,43 +1,59 @@
 import { useState } from "react"
+import RatingSelect from './RatingSelect'
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 
-export default function FeedbackForm() {
-    const [text, setText] = useState('')
-    const [btnDisable, setBtnDisable] = useState(true)
-    const [message, setMessage] = useState('')
+export default function FeedbackForm({ handleAdd }) {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisable, setBtnDisable] = useState(true)
+  const [message, setMessage] = useState('')
 
-    const handleTextChange = (e) => {
-      // Validation everytime we type somthing in
-      // If there is nothing on text, don't show it
+  const handleTextChange = (e) => {
+    // Validation everytime we type somthing in
+    // If there is nothing on text, don't show it
 
-      //Changed the validation for target
-      //cause text grabbed the value before the change.
-      //if (text === ""){
-      if (e.target.value === ""){
-        setBtnDisable(true)
-        setMessage(null)
-      }else if(e.target.value !== "" && e.target.value.trim().length <= 10){ //If the text has less than 10 characters
-        setBtnDisable(true)
-        setMessage('Text must be atleast 10 characters')
-      }else{ //The text has something and is more than 10 characters
-        setMessage(null)
-        setBtnDisable(false)
-      }
-      setText(e.target.value)   
+    //Changed the validation for target
+    //cause text grabbed the value before the change.
+    //if (text === ""){
+    if (e.target.value === "") {
+      setBtnDisable(true)
+      setMessage(null)
+    } else if (e.target.value !== "" && e.target.value.trim().length <= 10) { //If the text has less than 10 characters
+      setBtnDisable(true)
+      setMessage('Text must be atleast 10 characters')
+    } else { //The text has something and is more than 10 characters
+      setMessage(null)
+      setBtnDisable(false)
     }
+    setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedBack = {
+        text: text,
+        rating: rating
+      }
+      handleAdd(newFeedBack)
+      setText('')
+    }
+  }
   return (
     <Card>
-        <form action="">
-            <h2>How would you rate our server?</h2>
-            <div className="input-group">
-                <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                <Button type='submit' isDisable={btnDisable}>Send</Button>
-            </div>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate our server?</h2>
+        {/* Pass function as prop to set the rating */}
+        <RatingSelect select={(rating) => setRating(rating)}></RatingSelect>
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
+          <Button type='submit' isDisable={btnDisable}>Send</Button>
+        </div>
 
-            {/* Condition to make sure there is a message */}
-           {message && <div className="message">{message}</div>}
-        </form>
+        {/* Condition to make sure there is a message */}
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
   )
 }
